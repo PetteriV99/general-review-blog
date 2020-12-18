6ae26a1af54298dbaa95ef7cb0e756992db9d100
apbd <60779371+apbd@users.noreply.github.com>
2020-12-18 18:56:58 +0200

    first albert branch commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     DEBUG = True
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://petteri:Salasana12@localhost:5432/general-blog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://albert:albert@localhost:5432/review-blog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = 'super-secret-key'
