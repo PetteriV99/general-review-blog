29b409b7d416421ff029d3d0dfe76d26ec10a141
Petteri Vänttinen <58398715+PetteriV99@users.noreply.github.com>
2021-01-06 16:35:33 +0200

    updated requirments.txt

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     DEBUG = True
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://your_name:your_password@localhost:5432/smilecook'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://petteri:Salasana12@localhost:5432/general-blog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = 'super-secret-key'
